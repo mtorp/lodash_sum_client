4f4ebf6bffd6233c429d42ed9421efc28b19617f
Martin Torp <toldamtorp@gmail.com>
2021-09-21 14:10:24 +0200

    update

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const lodash = require('lodash');
 const thisArg = { p: 42 };
 
-lodash.sum([1, 2, 3], function (x) { return x + this.p }, thisArg); 
-lodash.sum({x : {p: 42}, y: {p: '43'}}, 'p');
+// 330
+console.log(lodash.sum([1, '2', 3], function (x) { return x + this.p }, thisArg)); 
+// 85
+console.log(lodash.sum({x : {p: 42}, y: {p: '43'}}, 'p'));
